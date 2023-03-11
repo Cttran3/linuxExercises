f7639869b700b304f27dd6e1d01568b5cfe3e84b
Camille <Cttran3@wisc.edu>
2023-03-10 18:08:49 -0600

    updated

--- school.sh
+++ school.sh
@@ -1,4 +1,16 @@
-Sum: 24154170100
-Average: /0
-Sum: 24154170100
-Average: 346988
+#!/bin/bash
+
+# select lines containing "MADISON SCHOOLS" and TotalAssessedValue column, then compute sum and average
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | { 
+    sum=0
+    count=0
+    while read value; do
+        sum=$((sum + value))
+        count=$((count + 1))
+    done
+    echo "Sum of TotalAssessedValue for MADISON SCHOOLS properties: $sum"
+    echo "Average TotalAssessedValue for MADISON SCHOOLS properties: $((sum/count))"
+}
+
+# ignore file in git
+echo "Property_Tax_Roll.csv" > .gitignore
